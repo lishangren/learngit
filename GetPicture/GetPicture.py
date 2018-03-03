e5e29b541587aa7852bd67bb61574097f5f4c9af
lishangren25 <lishangren25@163.com>
2018-03-03 17:38:07 +0800

    加强图片获取

--- GetPicture/GetPicture.py
+++ GetPicture/GetPicture.py
@@ -1,3 +1,5 @@
+#coding='utf-8'
+
 import datetime
 import urllib.request
 import re
@@ -5,6 +7,10 @@ import os, sys
 
 # url = "http://findicons.com/pack/2787/beautiful_flat_icons"
 url = "http://www.meizitu.com"
+# 设置headers，网站会根据这个判断你的浏览器及操作系统，很多网站没有此信息将拒绝你访问
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 UBrowser/6.1.2107.204 Safari/537.36'}
+# header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
+
 save_dir = "d:/spider_image/"
 
 # 获取要爬取的母网页的 HTML 内容
@@ -61,22 +67,20 @@ def spider_pic():
             continue;
 
         # 加上 HTTP 数据请求头
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
-        req = urllib.request.Request(url=iturl, headers=headers)
+
+        req = urllib.request.Request(url=iturl, headers=header)
         web = urllib.request.urlopen(req)
         img_data = web.read()  # 图片数据
 
-        date_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+        date_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")  # 当前时间
+        # 只下载30张图片
         if (cnt <= 30):
             f = open(save_dir + date_now + '.jpg', "wb")
             f.write(img_data)
             f.close()
         cnt = cnt + 1
 
-def test(self):
-    html = getHTML(url)
 
-    # sp = getPicList(html)
 
 if __name__=='__main__':
     spider_pic()
